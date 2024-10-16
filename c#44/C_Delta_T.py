857c52d181c7304f07fc01c148f8379005cc4cdc
chera-mihiretu <cheramihiretucm@gmail.com>
2024-10-16 13:09:59 -0400

    add oct 16

--- c#44/C_Delta_T.py
+++ c#44/C_Delta_T.py
@@ -6,15 +6,21 @@ def solution():
     l,r,k = [int(i) for i in input().split()]
     a,b = [int(i) for i in input().split()]
 
-
-    if abs(l- r) < k:
-        print(-1)
-    else:
-
-        
+    
+    a, b = min(a,b), max(a,b)
+    
+    if a == b:
+        return 0
+    if (r  - l < k) or (abs(a - l) < k  and abs(r - a) < k) or (abs(b - l) < k  and abs(r - b) < k):
+        return -1
+    elif b - a >= k:
+        return 1
+    elif b - a < k and a - l >= k or r - b >= k:
+        return 2
+    return 3
 
 # run the code
 if __name__ == '__main__':
     test_case = int(input())
     for i in range(test_case):
-        solution()
+        print(solution())
